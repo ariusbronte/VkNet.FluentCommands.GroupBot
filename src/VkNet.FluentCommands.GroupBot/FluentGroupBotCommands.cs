30de872eef790d6acb7d9f0e63146c836fd00f2b
Arius Bronte <ariusbronte@gmail.com>
2020-02-16 18:55:45 +0000

    Added message receiver

--- src/VkNet.FluentCommands.GroupBot/FluentGroupBotCommands.cs
+++ src/VkNet.FluentCommands.GroupBot/FluentGroupBotCommands.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using VkNet.Abstractions;
+using VkNet.Enums.SafetyEnums;
 using VkNet.Model;
 using VkNet.Model.GroupUpdate;
+using VkNet.Model.RequestParams;
 
 // ReSharper disable MemberCanBeProtected.Global
 // ReSharper disable MemberCanBePrivate.Global
@@ -121,6 +125,90 @@ namespace VkNet.FluentCommands.GroupBot
             }
 
             _textCommands.TryAdd((tuple.pattern, tuple.options), func);
+        }
+
+        /// <summary>
+        ///     Starts receiving messages.
+        /// </summary>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        public async Task ReceiveMessageAsync(CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            var longPollServer = await GetLongPollServerAsync(_longPollConfiguration.GroupId, cancellationToken);
+            var server = longPollServer.Server;
+            var ts = longPollServer.Ts;
+            var key = longPollServer.Key;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var longPollHistory = await GetBotsLongPollHistoryAsync(key, server, ts, _longPollConfiguration.Wait, cancellationToken);
+
+                if (longPollHistory?.Updates == null)
+                {
+                    continue;
+                }
+
+                Parallel.ForEach(longPollHistory.Updates, async update =>
+                {
+                    if (update.Type == GroupUpdateType.MessageNew)
+                    {
+                        var command = _textCommands
+                            .Where(x => Regex.IsMatch(update.MessageNew.Message.Text, x.Key.Item1, x.Key.Item2))
+                            .Select(x => x.Value)
+                            .SingleOrDefault();
+
+                        if (command == null)
+                        {
+                            return;
+                        }
+
+                        await command(_botClient, update, cancellationToken);
+                    }
+                });
+                
+                ts = longPollHistory.Ts;
+            }
+        }
+
+        /// <summary>
+        ///     Get data for the connection
+        /// </summary>
+        /// <param name="groupId">Group identifier.</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns>Returns data for the connection to long poll</returns>
+        private async Task<LongPollServerResponse> GetLongPollServerAsync(
+            ulong groupId,
+            CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return await _botClient.Groups.GetLongPollServerAsync(groupId);
+        }
+
+        /// <summary>
+        ///     Get group events.
+        /// </summary>
+        /// <param name="key">A secret session key.</param>
+        /// <param name="server">Server address.</param>
+        /// <param name="ts">the number of the last event to start receiving data from.</param>
+        /// <param name="wait">Wait time</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns>Returns group events.</returns>
+        private async Task<BotsLongPollHistoryResponse> GetBotsLongPollHistoryAsync(
+            string key,
+            string server,
+            string ts,
+            int wait,
+            CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return await _botClient.Groups.GetBotsLongPollHistoryAsync(new BotsLongPollHistoryParams
+            {
+                Key = key,
+                Server = server,
+                Ts = ts,
+                Wait = wait
+            });
         }
     }
 }
